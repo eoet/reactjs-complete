5b8719aabae28ebf29366a468484a2ee6024251e
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2020-11-12 22:48:48 +0700

    7. Handling user input

---
+++ pics/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
+
+export default function App() {
+  return (
+    <div className="ui container" style={{marginTop: '10px'}}>
+      <SearchBar />
+    </div>
+  );
+}
